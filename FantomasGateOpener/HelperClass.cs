617f077a4bfe37e2636282014e25ef9d2673fe01
Koča Maksimović <koca.maksimovic@outlook.com>
2021-09-14 20:31:32 +0200

    ...

---
+++ FantomasGateOpener/HelperClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace FantomasGateOpener
+{
+    public static class HelperClass
+    {
+        public static void AppendText(this RichTextBox box, string text, string color)
+        {
+            BrushConverter bc = new BrushConverter();
+
+            Application.Current.Dispatcher.Invoke((Action)(() =>
+            {
+                // your code here.
+                TextRange tr = new TextRange(box.Document.ContentEnd, box.Document.ContentEnd);
+                tr.Text = text;
+                try
+                {
+                    tr.ApplyPropertyValue(TextElement.ForegroundProperty,
+                        bc.ConvertFromString(color));
+                }
+                catch (FormatException) { }
+            }));
+
+            // Scroll to the end.
+            box.ScrollToEnd();
+        }
+    }
+}
